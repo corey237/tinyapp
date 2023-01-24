5102c7b5605a52b743db6f33fac0fa34698568cb
Corey Silver <coreysilver@outlook.com>
2023-01-23 21:28:26 -0500

    Added basic handler for POST request from new URL form, added function to generate a random 6 character string

--- express_server.js
+++ express_server.js
@@ -1,13 +1,20 @@
+const generateRandomString = function () {
+  return Math.random().toString(20).substr(2, 6);
+}
+
+
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
-}
+};
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -15,22 +22,31 @@ app.get('/', (req, res) => {
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
-})
+});
 
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
+});
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 })
 
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
   res.render('urls_show', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
-})
+});
