2a16fca4cea2865df7af5b79f1b045e9c3da9c6b
Corey Silver <coreysilver@outlook.com>
2023-01-25 15:58:58 -0500

    Added error handlers for registration page (Return 400 error codes for empty email/password fields or if the user already exists, also adjusted organization of routes (grouped GET's and POST's together)

--- express_server.js
+++ express_server.js
@@ -1,29 +1,52 @@
-const cookieParser = require("cookie-parser");
-
-const generateRandomString = function () {
-  return Math.random().toString(20).substr(2, 6);
-};
-
+//LOAD EXPRESS AND SET PORT NUMBER
 const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs");
+//FUNCTION FOR GENERATING RANDOM SHORTURL
+const generateRandomString = function () {
+  return Math.random().toString(20).substr(2, 6);
+};
 
+//FUNCTION FOR FINDING A USER BASED ON EMAIL
+
+const findUser = function (email) {
+  for (let user in users) {
+    if (email === users[user].email) {
+      return users[user];
+    }
+  }
+  return null;
+};
+
+//MIDDLEWARE
+const cookieParser = require("cookie-parser");
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+//DATABASES (URLS & USERS)
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+//GET ROUTES
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
@@ -31,18 +54,34 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]],
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]],
+  };
   res.render("./user_registration", templateVars);
   console.log(req.body);
 });
 
+app.get("/urls.json", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]],
+  };
+  res.json(urlDatabase, templateVars);
+});
+
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    user: users[req.cookies["user_id"]],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
@@ -53,11 +92,12 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_show", templateVars);
 });
 
+//POST ROUTES
 app.post("/urls", (req, res) => {
   const randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
@@ -65,12 +105,34 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+  res.cookie("user_id", req.body.email);
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
+  res.redirect("/urls");
+});
+
+app.post("/register", (req, res) => {
+  //If the email or password is empty, return an error
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send("Invalid Email or Password");
+  }
+  //If the user already exists return an error, otherwise continue with user creation
+  const doesUserExist = findUser(req.body.email);
+  if (doesUserExist !== null) {
+    return res.status(400).send("User already exists");
+  }
+  //Generate random user ID
+  const userID = generateRandomString();
+  //Create new user object based on submitted form and add it to users database
+  users[userID] = {
+    id: userID,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  res.cookie("user_id", userID);
   res.redirect("/urls");
 });
 
@@ -83,6 +145,8 @@ app.post("/urls/:id/update", (req, res) => {
   urlDatabase[req.params.id] = req.body.newURL;
   res.redirect("/urls");
 });
+
+//RUN WHEN SERVER IS STARTED
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}`);
